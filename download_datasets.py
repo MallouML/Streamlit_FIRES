d92f7367322da1cd213e485a508926e06ec7d0f3
MallouML <maelle.filopon@gmail.com>
2025-09-07 22:08:25 +0200

    Update download_datasets.py

--- download_datasets.py
+++ download_datasets.py
@@ -5,9 +5,9 @@ DATA_DIR = "Datasets"
 os.makedirs(DATA_DIR, exist_ok=True)
 
 datasets = {
-    "US_wildfire_weather_data.csv": "https://drive.google.com/uc?id=ID1",
-    "fires.csv": "https://drive.google.com/uc?id=ID2",
-    "dataset_v2.csv": "https://drive.google.com/uc?id=ID3"
+    "US_wildfire_weather_data.csv": "https://drive.google.com/file/d/1FfGg56InKnFMUcF8WFGQcFjWUMCB7dVY/view?usp=drive_link",
+    "fires.csv": "https://drive.google.com/file/d/1IhF1oK34yVs-gxsh8et8DsNHwOI5FZDc/view?usp=drive_link",
+    "dataset_v2.csv": "https://drive.google.com/file/d/1FXfZQpwLOX-3qoh0d2r1IiapZrr4y7NS/view?usp=drive_link"
 }
 
 def download_all():
