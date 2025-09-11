8cd68a357014bed1f10608fb7d23befc0ea7d9db
MallouML <maelle.filopon@gmail.com>
2025-09-11 17:34:33 +0200

    Update download_datasets.py

--- download_datasets.py
+++ download_datasets.py
@@ -5,18 +5,21 @@ DATA_DIR = "Datasets"
 os.makedirs(DATA_DIR, exist_ok=True)
 
 datasets = {
-    "US_wildfire_weather_data.csv": "https://drive.google.com/file/d/1FfGg56InKnFMUcF8WFGQcFjWUMCB7dVY/view?usp=drive_link",
-    "fires.csv": "https://drive.google.com/file/d/1IhF1oK34yVs-gxsh8et8DsNHwOI5FZDc/view?usp=drive_link",
-    "dataset_v2.csv": "https://drive.google.com/file/d/1FXfZQpwLOX-3qoh0d2r1IiapZrr4y7NS/view?usp=drive_link"
+    "US_wildfire_weather_data.csv": "1FfGg56InKnFMUcF8WFGQcFjWUMCB7dVY",
+    "fires.csv": "1IhF1oK34yVs-gxsh8et8DsNHwOI5FZDc",
+    "dataset_v2.csv": "1FXfZQpwLOX-3qoh0d2r1IiapZrr4y7NS"
 }
 
 def download_all():
-    for filename, url in datasets.items():
+    for filename, file_id in datasets.items():
         filepath = os.path.join(DATA_DIR, filename)
         if not os.path.exists(filepath):
             print(f"Téléchargement de {filename}...")
+            url = f"https://drive.google.com/uc?id={file_id}"
             gdown.download(url, filepath, quiet=False)
             print(f"{filename} téléchargé !")
         else:
             print(f"{filename} existe déjà, skip.")
 
+if __name__ == "__main__":
+    download_all()
