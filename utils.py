15f0dd355b13e129a0af9fe20b894a41b4101cc3
MallouML <maelle.filopon@gmail.com>
2025-09-17 09:27:22 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,9 +6,16 @@ DATA_DIR = "Datasets"
 
 def load_data(filename: str):
     """
-    Charge un fichier Parquet depuis le dossier Datasets.
+    Charge un fichier Parquet depuis le dossier Datasets
+    ou une URL externe.
     """
+    # Si c'est une URL (http/https)
+    if filename.startswith("http"):
+        return pd.read_parquet(filename)
+
+    # Sinon, chemin local
     filepath = os.path.join(DATA_DIR, filename)
     if not os.path.exists(filepath):
-        raise FileNotFoundError(f"{filepath} est introuvable.")
+        raise FileNotFoundError(f"{filepath} est introuvable dans {DATA_DIR}/")
+
     return pd.read_parquet(filepath)
